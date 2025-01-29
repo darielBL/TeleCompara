908cb0e95147d9cab39bd5a69b67e127b1009dda
Dariel Barcelo <darielbarcelo417@gmail.com>
2025-01-28 23:39:41 -0500

    check point

--- res/misc.js
+++ res/misc.js
@@ -1,12 +1,14 @@
-window.addEventListener('scroll', function () {
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+window.addEventListener('scroll', function() {
     const navbar = document.getElementById('navbar');
     if (window.scrollY > 100) { // Cambia 100 por la posición deseada
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
     }
-});
-document.getElementById("menu-toggle").addEventListener("click", function () {
-    const navLinks = document.getElementById("nav-links");
-    navLinks.classList.toggle("show");
 });
